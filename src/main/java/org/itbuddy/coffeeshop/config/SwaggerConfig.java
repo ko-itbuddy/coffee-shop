6b239766362a72109fd026c34e24cfe55b939c84
나평근 <itbuddy@pineone.com>
2024-08-22 06:07:00 +0900

    1. swagger 설정 수정 2. local DB 접속 정보 수정 3. Kafka, redis, mysql docker-compose 파일 추가

--- src/main/java/org/itbuddy/coffeeshop/config/SwaggerConfig.java
+++ src/main/java/org/itbuddy/coffeeshop/config/SwaggerConfig.java
@@ -17,9 +17,9 @@ public class SwaggerConfig {
 
     private Info apiInfo() {
         return new Info()
-            .title("API Test") // API의 제목
-            .description("Let's practice Swagger UI") // API에 대한 설명
-            .version("1.0.0"); // API의 버전
+            .title("COFFEE SHOP") // API의 제목
+            .description("커피샵 주문 시스템") // API에 대한 설명
+            .version("0.0.01"); // API의 버전
     }
 }
 
