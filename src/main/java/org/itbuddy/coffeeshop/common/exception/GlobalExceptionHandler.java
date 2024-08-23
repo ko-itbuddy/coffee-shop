49eb7bfd7e81a3cdc22b95594235dda7d4d0c0c0
나평근 <skvudrms54@gmail.com>
2024-08-23 09:31:50 +0900

    feat: Error 처리에  Internal Server 에러 처리 추가

--- src/main/java/org/itbuddy/coffeeshop/common/exception/GlobalExceptionHandler.java
+++ src/main/java/org/itbuddy/coffeeshop/common/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
-package org.itbuddy.coffeeshop.config;
+package org.itbuddy.coffeeshop.common.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.itbuddy.coffeeshop.common.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindException;
@@ -8,9 +9,11 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.HttpClientErrorException.BadRequest;
 
+@Slf4j
 @RestControllerAdvice
-public class ExceptionConfig {
+public class GlobalExceptionHandler {
 
+    // 컨트롤러 단 Validation
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(BindException.class)
     public ApiResponse<Object> bindException(BindException e) {
@@ -21,11 +24,25 @@ public class ExceptionConfig {
         );
     }
 
+    // 서비스, 도메인단 Validation
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(BadRequest.class)
-    public ApiResponse<Object> BadRequest(BadRequest e) {
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ApiResponse<Object> BadRequest(IllegalArgumentException e) {
         return ApiResponse.of(
             HttpStatus.BAD_REQUEST,
+            e.getMessage(),
+            null
+        );
+    }
+
+
+    // 이외의 오류
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public ApiResponse<Object> BadRequest(Exception e) {
+        log.error("[ERROR] Internal Server Error, Message = {}", e.getMessage());
+        return ApiResponse.of(
+            HttpStatus.INTERNAL_SERVER_ERROR,
             e.getMessage(),
             null
         );
